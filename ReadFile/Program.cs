f879d3c4fe54919f41a67781cb1886d487c2679b
Haeretik <vera10mx@hotmail.com>
2020-09-02 22:44:02 -0500

    first

---
+++ ReadFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(@"C:\");
+            string[] lines = System.IO.File.ReadAllLines(@"Animals.txt");
+            foreach (string line in lines)
+                Console.WriteLine("The quick " + line + " jumps over the lazy dog ");
+        }
+    }
+}
